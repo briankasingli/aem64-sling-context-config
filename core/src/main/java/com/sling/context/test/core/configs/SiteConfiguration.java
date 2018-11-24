9ae55c7684d81f003ed06ca861b68522c6688b26
briankasingli <brian.kasing.li@gmail.com>
2018-11-24 11:40:13 +0000

    refactored some code

--- core/src/main/java/com/sling/context/test/core/configs/SiteConfiguration.java
+++ core/src/main/java/com/sling/context/test/core/configs/SiteConfiguration.java
@@ -3,15 +3,23 @@ package com.sling.context.test.core.configs;
 import org.apache.sling.caconfig.annotation.Configuration;
 import org.apache.sling.caconfig.annotation.Property;
 
-@Configuration(label="Website Configuration", description="Configuration can be made per website")
+@Configuration(
+    label = "Website Configuration",
+    description = "Configuration can be made per website")
 public @interface SiteConfiguration {
 
-    @Property(label="Website Color", description="Painting website color of your choice.")
-    String websiteColor();
+  @Property(
+      label = "Website Color",
+      description = "Painting website color of your choice.")
+  String websiteColor();
 
-    @Property(label="Contact Email Address", description="Sending an email using the web form will inform this email address.")
-    String contactEmailAddress();
+  @Property(
+      label = "Contact Email Address",
+      description = "Sending an email using the web form will inform this email address.")
+  String contactEmailAddress();
 
-    @Property(label="Store Number ID", description="Store number will be displayed bottom right of the page")
-    int storeID();
+  @Property(
+      label = "Store Number ID",
+      description = "Store number will be displayed bottom right of the page")
+  int storeID();
 }
--- core/src/main/java/com/sling/context/test/core/models/SiteConfigurationModel.java
+++ core/src/main/java/com/sling/context/test/core/models/SiteConfigurationModel.java
@@ -2,36 +2,37 @@ package com.sling.context.test.core.models;
 
 import com.day.cq.wcm.api.Page;
 import com.sling.context.test.core.configs.SiteConfiguration;
+
+import javax.annotation.PostConstruct;
+
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.caconfig.ConfigurationBuilder;
 import org.apache.sling.models.annotations.Model;
 import org.apache.sling.models.annotations.injectorspecific.ScriptVariable;
 
-import javax.annotation.PostConstruct;
-
 @Model(adaptables = SlingHttpServletRequest.class)
 public class SiteConfigurationModel {
 
 
-    @ScriptVariable(name = "currentPage")
-    private Page page;
+  @ScriptVariable(name = "currentPage")
+  private Page page;
 
-    private SiteConfiguration config = null;
+  private SiteConfiguration config = null;
 
-    @PostConstruct
-    private void init() {
-        config = page.adaptTo(ConfigurationBuilder.class).as(SiteConfiguration.class);
-    }
+  @PostConstruct
+  private void init() {
+    config = page.adaptTo(ConfigurationBuilder.class).as(SiteConfiguration.class);
+  }
 
-    public String getWebsiteColor() {
-        return config.websiteColor();
-    }
+  public String getWebsiteColor() {
+    return config.websiteColor();
+  }
 
-    public String getContactEmailAddress() {
-        return config.contactEmailAddress();
-    }
+  public String getContactEmailAddress() {
+    return config.contactEmailAddress();
+  }
 
-    public int getStoreID() {
-        return config.storeID();
-    }
+  public int getStoreID() {
+    return config.storeID();
+  }
 }
